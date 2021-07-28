14c7e2769bd09152c559931712a79001ab0e544e
Aaron <hourglassprograms@gmail.com>
2021-07-28 13:25:28 +0100

    Saving progress on listening/publishing of positio

--- src/commander/src/listener/listener.py
+++ src/commander/src/listener/listener.py
@@ -1,11 +1,29 @@
 #! /usr/bin/env python
 import rospy
+import std_msgs.msg
+
 from teb_local_planner.msg import FeedbackMsg
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
 
 
 def callback(path):
     print(path)
     print('------------------------------------------------------')
+
+
+def scallback(pos):
+    print(f"{pos}")
+
+    # Make goal publisher
+    pub = rospy.Publisher("/initalpose",
+                          PoseWithCovarianceStamped, queue_size=10)
+
+    p = PoseWithCovarianceStamped()
+    p.pose.pose.position.x = 2
+    p.pose.pose.position.y = 2
+    p.pose.pose.position.z = 0
+
+    pub.publish(p)
 
 
 def listener():
@@ -18,6 +36,10 @@ def listener():
 
     rospy.Subscriber(("/" + name + topic), FeedbackMsg, callback)
     rospy.loginfo(("listening to /" + name + topic))
+
+    rospy.Subscriber(("/" + name + "/setpos"),
+                     PoseStamped, scallback)
+    rospy.loginfo(("listening to /" + name + "/setpos"))
 
     # start listening for data
     rospy.spin()
--- src/teb_local_planner/scripts/publish_viapoints.py
+++ src/teb_local_planner/scripts/publish_viapoints.py
@@ -2,53 +2,50 @@
 
 # Author: christoph.roesmann@tu-dortmund.de
 
-import rospy, math
+import rospy
+import math
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Path
 
 
 def publish_via_points_msg():
-  pub = rospy.Publisher('/test_optim_node/via_points', Path, queue_size=1)
-  rospy.init_node("test_via_points_msg")
+    pub = rospy.Publisher('/test_optim_node/via_points', Path, queue_size=1)
+    rospy.init_node("test_via_points_msg")
+
+    via_points_msg = Path()
+    via_points_msg.header.stamp = rospy.Time.now()
+    via_points_msg.header.frame_id = "odom"  # CHANGE HERE: odom/map
+
+    # Add via-points
+    point0 = PoseStamped()
+    point0.pose.position.x = 0.0
+    point0.pose.position.y = 0.0
+
+    point1 = PoseStamped()
+    point1.pose.position.x = 5.0
+    point1.pose.position.y = -5.0
+
+    point2 = PoseStamped()
+    point2.pose.position.x = 25.0
+    point2.pose.position.y = -5.0
+
+    point3 = PoseStamped()
+    point3.pose.position.x = 30
+    point3.pose.position.y = 0
+
+    via_points_msg.poses = [point0, point1, point2, point3]
+
+    r = rospy.Rate(5)  # 10hz
+    t = 0.0
+    while not rospy.is_shutdown():
+
+        pub.publish(via_points_msg)
+
+        r.sleep()
 
 
-  via_points_msg = Path() 
-  via_points_msg.header.stamp = rospy.Time.now()
-  via_points_msg.header.frame_id = "odom" # CHANGE HERE: odom/map
-  
-  # Add via-points
-  point0 = PoseStamped()
-  point0.pose.position.x = 0.0;
-  point0.pose.position.y = 0.0;
-  
-  point1 = PoseStamped()
-  point1.pose.position.x = 5.0;
-  point1.pose.position.y = -5.0;
-  
-  point2 = PoseStamped()
-  point2.pose.position.x = 25.0;
-  point2.pose.position.y = -5.0;
-
-  point3 = PoseStamped()
-  point3.pose.position.x = 30;
-  point3.pose.position.y = 0;
-
-
-  via_points_msg.poses = [point0, point1, point2, point3]
-
-  r = rospy.Rate(5) # 10hz
-  t = 0.0
-  while not rospy.is_shutdown():
-        
-    pub.publish(via_points_msg)
-    
-    r.sleep()
-
-
-
-if __name__ == '__main__': 
-  try:
-    publish_via_points_msg()
-  except rospy.ROSInterruptException:
-    pass
-
+if __name__ == '__main__':
+    try:
+        publish_via_points_msg()
+    except rospy.ROSInterruptException:
+        pass
